9e9f259f9fb0d60fec2bc7815fac5a7324a9aeb5
Chantal Justamond <cjustamm66@me.com>
2017-12-05 15:51:41 -0700

    first palindrome solution was wrong. recursion solution working appropiately

--- palindrome/palindrome.rb
+++ palindrome/palindrome.rb
@@ -7,14 +7,19 @@
 # //   palindrome("abba") == true
 # //   palindrome("abcdefg") == false
 
-  def palindrome?(str)
-    str = str.downcase
-    if str == str.reverse
-      true
-    else
-      false
-    end
+
+def palindrome?(str)
+  str = str.downcase
+
+  if str.length == 0 || str.length == 1
+    true
+  elsif str[0] == str[-1]
+    palindrome?(str[1..-2])
+    true
+  else
+    false
   end
+end
 
 a = 'abba'
 b = 'abcdefg'
