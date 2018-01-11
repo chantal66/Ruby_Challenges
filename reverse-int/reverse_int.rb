17b7296a78e4687ab6961c514ac451da2a3951e7
Chantal Justamond <cjustamm66@me.com>
2018-01-11 14:56:09 -0700

    added a reverse to string method so we can reverse an int without reverse method

--- reverse-int/reverse_int.rb
+++ reverse-int/reverse_int.rb
@@ -8,6 +8,15 @@
 # //   reverseInt(-15) === -51
 # //   reverseInt(-90) === -9
 
+# def reverse_int(n)
+#   reversed = n.to_s.reverse.to_i
+
+#   if n < 0
+#     reversed * -1
+#   else
+#     reversed
+#   end
+# end
 def reverse_int(n)
   reversed = n.to_s.reverse.to_i
 
@@ -19,8 +28,31 @@ def reverse_int(n)
 end
 
 
-p reverse_int(15) === 51
-p reverse_int(981) === 189
-p reverse_int(500) === 5
-p reverse_int(-15) === -51
-p reverse_int(-90) === -9
+def reversed_str(str)
+  str = str.downcase
+  if str.length < 2
+    str
+  else
+    str[-1] + reversed_str(str[1..-2]) + str[0]
+  end
+end
+
+def reverse_int(n)
+
+  number_to_s = n.to_s
+  reversed_number = reversed_str(number_to_s).to_i
+  if n < 0
+    reversed_number * -1
+  else
+    reversed_number
+  end
+end
+
+
+
+
+p reverse_int(15)
+p reverse_int(981)
+p reverse_int(500)
+p reverse_int(-15)
+p reverse_int(-90)
