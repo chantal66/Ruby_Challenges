e0c8482b44706402218586a85974808fcfd2d642
Chantal Justamond <cjustamm66@me.com>
2018-01-22 13:57:56 -0700

    a more explicit check for values in char_freq

--- contains_duplicate/contains_duplicate.rb
+++ contains_duplicate/contains_duplicate.rb
@@ -5,7 +5,7 @@
 def contains_duplicate(arr)
   chars_freq = Hash.new(0)
   arr.each { |char| chars_freq[char] += 1 }
-  if chars_freq.values.include? 2
+  if chars_freq.values.max >= 2
     true
   else
     false
@@ -14,3 +14,4 @@ end
 
 
 p contains_duplicate([1,3,4,5,6,3,5,7,4,7,8,8])
+p contains_duplicate([7,10,5,5,6,6,4,10,5,4,9,4,9,6,5,9,6,3,6,5,6,7,7,4,9,9,10,5,8,1,8,3,2,7,5,10,1,8,5,8,4,3,6,4,9,4,2,8,3,2,2,1,5,6,3,2,6,1,8,6,2,9,1,4,5,10,8,5,10,5,10,1,4,8,3,6,4,10,9,1,1,1,2,2,9,6,6,8,1,9,2,5,5,2,1,8,5,2,3,10])
