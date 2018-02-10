3ce9e7f84ff7a075aac9787f6728ac9635e4cd1d
Chantal Justamond <cjustamm66@me.com>
2018-02-10 11:52:36 -0700

    sum , substract and division working :heavy_check_mark: multiplication pending

--- termulator/termulator.rb
+++ termulator/termulator.rb
@@ -3,3 +3,28 @@
 input_array = ARGV
 
 p input_array
+
+def sum(input_array)
+  input_array[0].to_i + input_array[2].to_i
+end
+
+def substract(input_array)
+  input_array[0].to_i - input_array[2].to_i
+end
+
+def multiply(input_array)
+  input_array[0].to_i * input_array[2].to_i
+end
+
+def division(input_array)
+  input_array[0].to_i / input_array[2].to_i
+end
+
+puts case
+       when input_array[1] == '+' then sum(input_array)
+       when input_array[1] == '-' then substract(input_array)
+       when input_array[1] == '*' then multiply(input_array)
+       when input_array[1] == '/' then multiply(input_array)
+       else
+         "you didn't give a number"
+     end
