05e13e50a1c315a281e36f38a9812d7848b95705
Chantal Justamond <cjustamm66@me.com>
2018-03-07 12:58:18 -0700

    better way to create an alphabet with values

--- love_vs_friendship/love_vs_friendship.rb
+++ love_vs_friendship/love_vs_friendship.rb
@@ -7,9 +7,10 @@
 # So friendship is twice stronger than love :-)
 #
 # The input will always be in lowercase and never be empty.
+#lookup = { 'a' =>1, 'b'=> 2, 'c' => 3, 'd'=>4, 'e'=>5, 'f' => 6, 'g'=>7, 'h'=>8, 'i'=>9, 'j'=>10, 'k'=>11, 'l'=>12, 'm'=>13, 'n'=>14, 'o'=>15, 'p'=>16, 'q'=>17, 'r'=>18, 's'=>19, 't'=>20, 'u'=>21, 'v'=>22, 'w'=>23, 'x'=>24, 'y'=>25, 'z'=>26 }
 
 def words_to_marks(string)
-  lookup = { 'a' =>1, 'b'=> 2, 'c' => 3, 'd'=>4, 'e'=>5, 'f' => 6, 'g'=>7, 'h'=>8, 'i'=>9, 'j'=>10, 'k'=>11, 'l'=>12, 'm'=>13, 'n'=>14, 'o'=>15, 'p'=>16, 'q'=>17, 'r'=>18, 's'=>19, 't'=>20, 'u'=>21, 'v'=>22, 'w'=>23, 'x'=>24, 'y'=>25, 'z'=>26 }
+  lookup = Hash[('a'..'z').to_a.zip(1..26).to_a]
   splitted = string.split('')
   splitted.map { |char| lookup[char] }.reduce(:+)
 end
